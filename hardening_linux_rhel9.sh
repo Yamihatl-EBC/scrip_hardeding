b3fcd92d0aa3dc370d7ae5eabf01f186a2067d37
Uriel Yamihatl Gamboa Gonzalez <144174938+Yamihatl-EBC@users.noreply.github.com>
2023-11-08 16:02:10 -0600

    Update hardening_linux_rhel9.sh

--- hardening_linux_rhel9.sh
+++ hardening_linux_rhel9.sh
@@ -4,9 +4,9 @@ echo "Actualizando paquetes"
 yum update -y
 yum install -y wget 
 yum install -y policycoreutils-python
-sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-sudo dnf list docker-ce
-sudo dnf install docker-ce --nobest -y
+#sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+#sudo dnf list docker-ce
+#sudo dnf install docker-ce --nobest -y
 sudo yum install ruby -y
 cd /home/ec2-user
 wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
@@ -14,14 +14,14 @@ chmod +x ./install
 sudo ./install auto
 sudo service codedeploy-agent status
 sudo service codedeploy-agent start
-sudo systemctl start docker
-sudo systemctl enable docker
-cd /etc/docker/
+#sudo systemctl start docker
+#sudo systemctl enable docker
+#cd /etc/docker/
 wget https://raw.githubusercontent.com/Yamihatl-EBC/scrip_hardeding/dev/daemon.json
 chmod 640 /etc/docker/daemon.json
-mkdir /u01/docker_installation
-sudo systemctl daemon-reload
-systemctl restart docker	
+#mkdir /u01/docker_installation
+#sudo systemctl daemon-reload
+#systemctl restart docker	
 cd /
 
 #Instalando firewalld

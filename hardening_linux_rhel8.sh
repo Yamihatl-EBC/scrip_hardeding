608579dadcfaca47e9f29a404542807016e8d8b8
Uriel Yamihatl Gamboa Gonzalez <144174938+Yamihatl-EBC@users.noreply.github.com>
2024-02-06 10:50:13 -0600

    Update hardening_linux_rhel8.sh

--- hardening_linux_rhel8.sh
+++ hardening_linux_rhel8.sh
@@ -2,6 +2,8 @@
 #Paquetes
 echo "Actualizando paquetes"
 sudo dnf install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+sudo systemctl enable amazon-ssm-agent
+sudo systemctl start amazon-ssm-agent
 yum update -y
 yum install -y wget 
 yum install java -y
